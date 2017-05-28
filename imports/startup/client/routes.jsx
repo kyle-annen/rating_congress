ca48ba7841a1338464837bd7537772d580aa2632
Kyle Annen <kyleannen@gmail.com>
2017-05-28 12:16:06 -0500

    implement simple routing

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import { Index } from '../../ui/components/index.jsx';
 
-Meteor.startup( () => {
+import { About } from '../../ui/pages/about.jsx';
+import { Contact } from '../../ui/pages/contact.jsx';
 
-    render(
-        <BrowserRouter> 
-            <Route path="/" component={ Index } />
-        </BrowserRouter>,
-        document.getElementById('react-root')
-    );
+Meteor.startup( () => {
+	render(
+		<BrowserRouter>
+				<div>
+					<Route exact path="/" component={ Index } />
+					<Route path="/about" component={ About } />
+					<Route path="/contact" component={ Contact } />
+				</div>
+		</BrowserRouter>,
+		document.getElementById('react-root')
+	);
 })
 
--- imports/ui/components/index.jsx
+++ imports/ui/components/index.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export const Index = () => <h3>Index</h3>;
+export const Index = () => (
+  <h1>Index</h1>
+)
