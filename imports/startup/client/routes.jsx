e401d9e990600862c570bff1e962e2287ad78688
Kyle Annen <kyleannen@gmail.com>
2017-05-28 13:08:26 -0500

    implement simple routing

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import { Index } from '../../ui/components/index.jsx';
+import { Header } from '../../ui/layouts/header.jsx';
 
 import { About } from '../../ui/pages/about.jsx';
 import { Contact } from '../../ui/pages/contact.jsx';
@@ -10,11 +11,14 @@ import { Contact } from '../../ui/pages/contact.jsx';
 Meteor.startup( () => {
 	render(
 		<BrowserRouter>
-				<div>
+			<div>
+				<Header />
+				<div className="container">
 					<Route exact path="/" component={ Index } />
 					<Route path="/about" component={ About } />
 					<Route path="/contact" component={ Contact } />
 				</div>
+			</div>
 		</BrowserRouter>,
 		document.getElementById('react-root')
 	);
